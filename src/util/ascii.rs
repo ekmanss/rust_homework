28a5f78dad6cd87e78517f0ad6d603f850d1472e
ekmanss <hfers@msn.com>
2023-07-12 08:55:29 +0800

    format

--- src/util/ascii.rs
+++ src/util/ascii.rs
@@ -16,7 +16,10 @@ static ASCII_LIST: Lazy<[char; 128]> = Lazy::new(|| {
 pub fn ascii_code_list(start_char: char, end_char: char) -> Result<Vec<u8>, ASCIIError> {
     // let arr: Vec<char> = (0..128).map(|i| i as u8 as char).collect();
     let mut result = Vec::new();
-    if let (Some(start_index), Some(end_index)) = (ASCII_LIST.iter().position(|&c| c == start_char), ASCII_LIST.iter().position(|&c| c == end_char)) {
+    if let (Some(start_index), Some(end_index)) = (
+        ASCII_LIST.iter().position(|&c| c == start_char),
+        ASCII_LIST.iter().position(|&c| c == end_char),
+    ) {
         let mut current_index = start_index;
         while current_index != end_index {
             result.push(ASCII_LIST[current_index] as u8);
@@ -32,15 +35,31 @@ pub fn ascii_code_list(start_char: char, end_char: char) -> Result<Vec<u8>, ASCI
     }
 }
 
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn test_print_chars_Ok() {
-        assert_eq!(ascii_code_list('a', 'Z'), Ok(vec![97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90]));
-        assert_eq!(ascii_code_list('A', 'a'), Ok(vec![65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97]));
+        assert_eq!(
+            ascii_code_list('a', 'Z'),
+            Ok(vec![
+                97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113,
+                114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 0, 1, 2, 3,
+                4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
+                26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46,
+                47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67,
+                68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88,
+                89, 90
+            ])
+        );
+        assert_eq!(
+            ascii_code_list('A', 'a'),
+            Ok(vec![
+                65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85,
+                86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97
+            ])
+        );
     }
 
     #[test]
